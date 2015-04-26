51a925af6af839eeb0e6d5e0ed16db0cf184f400
cornelius5241 <uucorneliu@gmail.com>
2015-04-26 21:49:58 +0300

    parsing function functionally for xml files

--- parser/src/Main.java
+++ parser/src/Main.java
@@ -3,6 +3,15 @@
 public class Main {
     public static void main(String args[])
     {
-              Parser pars = new Parser();
+        Parser parser = new Parser();
+        parser.parseByTutu();
+
+        for(int i=0;i<parser.getValues().length;i++){
+            System.out.println("########### File :"+i+"###############");
+            for(int j=0;j<parser.getValues()[i].length;j++){
+                System.out.println(parser.getValues()[i][j]);
+            }
+            System.out.println("################################");
+        }
     }
 }
--- parser/src/Parser.java
+++ parser/src/Parser.java
@@ -6,10 +6,64 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import java.io.File;
 import java.io.IOException;
 public class Parser
 {
-    Parser()
+    private String[][] values;
+    private File[] files;
+    private int nrOfFiles;
+    File directory;
+
+    public Parser(){
+        this.directory=new File("src\\file");//setam directorul
+        this.files=this.directory.listFiles();//listam toate fisierele
+        this.nrOfFiles=this.files.length;// initializam nr de fisiere
+        this.values=new String[this.nrOfFiles][];
+    }
+
+    public void parseByTutu(){
+
+        DocumentBuilderFactory documentBuilderFactorytory =DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder documentBuilder = documentBuilderFactorytory.newDocumentBuilder();
+            try {
+                int index=0;
+                for(File file : files) {
+
+                    Document document = documentBuilder.parse(file.getAbsoluteFile());//documentul pe care il parsam
+
+                    NodeList nodeList = document.getElementsByTagName("*");//lista cu toate tagurile
+
+                    System.out.println("List =" + nodeList.getLength());//numarul de taguri
+
+                    values[index]=new String[nodeList.getLength()];//initializam o linie din matrice cu nr de taguri
+
+                    for (int i = 1; i < nodeList.getLength(); i++)//pt fiecare tag
+                    {
+                        Element element = (Element) nodeList.item(i);//luam un tag
+                        //System.out.println(element.getNodeName());
+                        if (element.getTextContent().length() == 0) {
+                            values[index][i]="#";
+                           // System.out.println("not found");
+                        } else{
+                            //System.out.println(element.getTextContent());//numele valorii din tag
+                            values[index][i]=element.getTextContent();
+                             }
+                    }
+                }
+            } catch (SAXException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void parseMeByToto()
     {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
@@ -51,5 +105,9 @@ public class Parser
         {
             e.printStackTrace();
         }
+    }
+
+    public String[][] getValues() {
+        return values;
     }
 }
