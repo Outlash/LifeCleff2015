b7c52f34160b2f60c8ebbec614375ddedc5b0719
cornelius5241 <uucorneliu@gmail.com>
2015-04-27 11:48:00 +0300

    compiled + added output files

--- parser/src/ToSimpleFile.java
+++ parser/src/ToSimpleFile.java
@@ -12,9 +12,9 @@ public class ToSimpleFile {
         try {
             PrintWriter outputStream = new PrintWriter(fileName);
 
-            outputStream.println(values.length+" "+values[0].length);//scrien nr de seturi de valori + nr de valori dintr-un set
+            outputStream.println(values.length+" "+(values[0].length-1));//scrien nr de seturi de valori + nr de valori dintr-un set
 
-            for(int i=0;i<values.length;i++) {
+            for(int i=1;i<values.length;i++) {
                 for (int j = 0; j < values[i].length; j++)
                     outputStream.print(values[i][j] + " ");
                 outputStream.println();
